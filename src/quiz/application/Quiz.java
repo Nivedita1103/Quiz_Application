333b55e4f1988eb9527e80b94a9d756df0e25f6b
Nivedita <nskokil@gmail.com>
2023-05-14 22:43:02 +0530

    initial commit

---
+++ src/quiz/application/Quiz.java
@@ -0,0 +1,316 @@
+package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Quiz extends JFrame implements ActionListener {
+    
+    String questions[][] = new String[10][5];
+    String answers[][] = new String[10][2];
+    String useranswers[][] = new String[10][1];
+    JLabel qno, question;
+    JRadioButton opt1, opt2, opt3, opt4;
+    ButtonGroup groupoptions;
+    JButton next, submit, lifeline;
+    
+    public static int timer = 15;
+    public static int ans_given = 0;
+    public static int count = 0;
+    public static int score = 0;
+    
+    String name;
+    
+    Quiz(String name) {
+        this.name = name;
+        setBounds(50, 0, 1440, 850);
+        getContentPane().setBackground(Color.decode("#0B0B45"));
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/quiz.jpg"));
+        JLabel image = new JLabel(i1);
+        image.setBounds(0, 0, 1440, 392);
+        add(image);
+        
+        qno = new JLabel();
+        qno.setBounds(100, 450, 50, 30);
+        qno.setFont(new Font("Tahoma", Font.PLAIN, 24));
+        qno.setForeground(Color.white);
+        add(qno);
+        
+        question = new JLabel();
+        question.setBounds(150, 450, 900, 30);
+        question.setFont(new Font("Tahoma", Font.PLAIN, 24));
+        question.setForeground(Color.WHITE);
+        add(question);
+        
+        questions[0][0] = "Which of the following function is used to generate the application's top-level window?";
+        questions[0][1] = "JCombo";
+        questions[0][2] = "JFrame";
+        questions[0][3] = "JPanel";
+        questions[0][4] = "JRE";
+
+        questions[1][0] = "Among AWT and Swing which is platform independent?";
+        questions[1][1] = "awt";
+        questions[1][2] = "Swing";
+        questions[1][3] = "none";
+        questions[1][4] = "both a and b";
+
+        questions[2][0] = "Which of the following is not a Swing component?";
+        questions[2][1] = " JScrollPane";
+        questions[2][2] = "JTextArea";
+        questions[2][3] = "JPanel";
+        questions[2][4] = "JLablel";
+
+        questions[3][0] = "Which of the following methods is used to set the title of a JFrame?";
+        questions[3][1] = "setLabel()";
+        questions[3][2] = "setCaption()";
+        questions[3][3] = "setTitle()";
+        questions[3][4] = "setText()";
+
+        questions[4][0] = " Give the abbreviation of AWT?";
+        questions[4][1] = "Application Windowing Toolkit";
+        questions[4][2] = "Applet Windowing Toolkit";
+        questions[4][3] = "Absolute Windowing Toolkit";
+        questions[4][4] = "Abstract Windowing Toolkit";
+
+        questions[5][0] = "Which is a component in AWT that can contain another components like buttons, textfields, labels etc.?";
+        questions[5][1] = "Window";
+        questions[5][2] = " Container";
+        questions[5][3] = "Panel";
+        questions[5][4] = "Frame";
+
+        questions[6][0] = "Which of the following components can display images?";
+        questions[6][1] = " JLabel";
+        questions[6][2] = "JList";
+        questions[6][3] = "JFrame";
+        questions[6][4] = "Label";
+
+        questions[7][0] = "JDBC stands for?"; 
+        questions[7][1] = "Java Database Connect";
+        questions[7][2] = "Java Database Connectivity";
+        questions[7][3] = "Java Application Resource";
+        questions[7][4] = "Json Database Connect";
+
+        questions[8][0] = " How many types of JDBC drivers?";
+        questions[8][1] = "4";
+        questions[8][2] = "3";
+        questions[8][3] = "2";
+        questions[8][4] = "1";
+
+        questions[9][0] = "Which of the following is used to find and fix bugs in the program?";
+        questions[9][1] = "JDB";
+        questions[9][2] = "JVM";
+        questions[9][3] = "JDK";
+        questions[9][4] = "JRE";
+        
+        answers[0][1] = "JFrame";
+        answers[1][1] = "Swing";
+        answers[2][1] = "JScrollPanel";
+        answers[3][1] = "setTitle()";
+        answers[4][1] = "Abstract Windowing Toolkit";
+        answers[5][1] = "Container";
+        answers[6][1] = "JLabel";
+        answers[7][1] = "Java Database Connectivity";
+        
+        answers[8][1] = "4";
+        answers[9][1] = "JDB";
+        
+        opt1 = new JRadioButton();
+        opt1.setBounds(170, 520, 700, 30);
+        opt1.setBackground(Color.WHITE);
+        opt1.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(opt1);
+        
+        opt2 = new JRadioButton();
+        opt2.setBounds(170, 560, 700, 30);
+        opt2.setBackground(Color.WHITE);
+        opt2.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(opt2);
+        
+        opt3 = new JRadioButton();
+        opt3.setBounds(170, 600, 700, 30);
+        opt3.setBackground(Color.WHITE);
+        opt3.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(opt3);
+        
+        opt4 = new JRadioButton();
+        opt4.setBounds(170, 640, 700, 30);
+        opt4.setBackground(Color.WHITE);
+        opt4.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(opt4);
+        
+        groupoptions = new ButtonGroup();
+        groupoptions.add(opt1);
+        groupoptions.add(opt2);
+        groupoptions.add(opt3);
+        groupoptions.add(opt4);
+        
+        next = new JButton("Next");
+        next.setBounds(1100, 550, 200, 40);
+        next.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        next.setBackground(new Color(30, 144, 255));
+        next.setForeground(Color.WHITE);
+        next.addActionListener(this);
+        add(next);
+        
+        lifeline = new JButton("50-50 Lifeline");
+        lifeline.setBounds(1100, 630, 200, 40);
+        lifeline.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        lifeline.setBackground(new Color(30, 144, 255));
+        lifeline.setForeground(Color.WHITE);
+        lifeline.addActionListener(this);
+        add(lifeline);
+        
+        submit = new JButton("Submit");
+        submit.setBounds(1100, 710, 200, 40);
+        submit.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        submit.setBackground(new Color(30, 144, 255));
+        submit.setForeground(Color.WHITE);
+        submit.addActionListener(this);
+        submit.setEnabled(false);
+        add(submit);
+        
+        start(count);
+        
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == next) {
+            repaint();
+            opt1.setEnabled(true);
+            opt2.setEnabled(true);
+            opt3.setEnabled(true);
+            opt4.setEnabled(true);
+            
+            ans_given = 1;
+            if (groupoptions.getSelection() == null) {
+               useranswers[count][0] = "";
+            } else {
+                useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+            }
+            
+            if (count == 8) {
+                next.setEnabled(false);
+                submit.setEnabled(true);
+            }
+            
+            count++;
+            start(count);
+        } else if (ae.getSource() == lifeline) {
+            if (count == 2 || count == 4 || count == 6 || count == 8 || count == 9) {
+                opt2.setEnabled(false);
+                opt3.setEnabled(false);
+            } else {
+                opt1.setEnabled(false);
+                opt4.setEnabled(false);
+            }
+            lifeline.setEnabled(false);
+        } else if (ae.getSource() == submit) {
+            ans_given = 1;
+            if (groupoptions.getSelection() == null) {
+                useranswers[count][0] = "";
+            } else {
+                useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+            }
+
+            for (int i = 0; i < useranswers.length; i++) {
+                if (useranswers[i][0].equals(answers[i][1])) {
+                    score += 10;
+                } else {
+                    score += 0;
+                }
+            }
+            setVisible(false);
+            new Score(name, score);
+        }
+    }
+    
+    public void paint(Graphics g) {
+        super.paint(g);
+        
+        String time = "Time left - " + timer + " seconds"; // 15
+        g.setColor(Color.RED);
+        g.setFont(new Font("Tahoma", Font.BOLD, 25));
+        
+        if (timer > 0) { 
+            g.drawString(time, 1100, 500);
+        } else {
+            g.drawString("Times up!!", 1100, 500);
+        }
+        
+        timer--; // 14
+        
+        try {
+            Thread.sleep(1000);
+            repaint();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        if (ans_given == 1) {
+            ans_given = 0;
+            timer = 15;
+        } else if (timer < 0) {
+            timer = 15;
+            opt1.setEnabled(true);
+            opt2.setEnabled(true);
+            opt3.setEnabled(true);
+            opt4.setEnabled(true);
+            
+            if (count == 8) {
+                next.setEnabled(false);
+                submit.setEnabled(true);
+            }
+            if (count == 9) { // submit button
+                if (groupoptions.getSelection() == null) {
+                   useranswers[count][0] = "";
+                } else {
+                    useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+                }
+                
+                for (int i = 0; i < useranswers.length; i++) {
+                    if (useranswers[i][0].equals(answers[i][1])) {
+                        score += 10;
+                    } else {
+                        score += 0;
+                    }
+                }
+                setVisible(false);
+                new Score(name, score);
+            } else { // next button
+                if (groupoptions.getSelection() == null) {
+                   useranswers[count][0] = "";
+                } else {
+                    useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+                }
+                count++; // 0 // 1
+                start(count);
+            }
+        }
+        
+    }
+    
+    public void start(int count) {
+        qno.setText("" + (count + 1) + ". ");
+        question.setText(questions[count][0]);
+        opt1.setText(questions[count][1]);
+        opt1.setActionCommand(questions[count][1]);
+        
+        opt2.setText(questions[count][2]);
+        opt2.setActionCommand(questions[count][2]);
+        
+        opt3.setText(questions[count][3]);
+        opt3.setActionCommand(questions[count][3]);
+        
+        opt4.setText(questions[count][4]);
+        opt4.setActionCommand(questions[count][4]);
+        
+        groupoptions.clearSelection();
+    }
+    
+    public static void main(String[] args) {
+        new Quiz("User");
+    }
+}
